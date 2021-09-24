555ae044c919fa91de726496d08d9febd5e1a5a7
Veronica F Reyes <veronica.fierro.reyes1@gmail.com>
2021-09-24 13:48:29 -0500

    Working on Zillow Clustering Project

---
+++ README.md
@@ -0,0 +1,189 @@
+# README for  Zillow Clustering Project
+# Zillow: What is driving the errors in the Zestimates?
+
+GOALS:
+-----
+- Uncover what the drivers of the error in the zestimate using Zillow data using clustering methodologies.
+- Define and use single unit / single family homes based on transaction data from May 1, 2017 through August 31, 2017,
+
+
+KEY FINDINGS & TAKEAWAYS:
+-------------------------
+ Modeling Takeaways:
+
+- Polynomial Regression Model performed better than the baseline by having a l Ir RMSE than the baseline.
+- The model explained 35.6% of the variance on out of sample data very similar to our validate dataset. 
+
+ Location & Tax Rates Takeaways:
+- All properties are located in the state of California
+- All properties are located in one of 3 counties:
+    - Los Angeles County
+    - Orange County
+    - Ventura County
+    
+- The max tax rate was 0.01885 was located in Orange County
+- The mix tax rate was 0.00622 was located in Orange County
+- The highest average tax rate was in Los Angeles County with an average of 0.013054
+- The l Ist average tax rate was Ventura County with an average of  0.011513
+- The highest number of properties I located in Los Angeles County with 21,469 properties
+- The l Ist number of properties I located in Ventura County with 3,189 properties
+
+
+DATA DICTIONARY:
+----------------
+
+|Target|Datatype|Definition|
+|:-------|:--------|:----------|
+| tax_value                  | |35078 non-null:: float| value of the property in dollars  |              |
+
+|Feature|Datatype|Definition|
+|:-------|:--------|:----------|
+|   bedrooms               |35078 non-null:  float64| number of bedrooms |              |
+|   bathrooms              |35078 non-null:  float64| number of bathrooms  |              |
+|   sq_footage             |35078 non-null:  float64| square footage of the building  |              |            |
+|   yr_built               |35078 non-null:  float64| year property was built
+|   taxamount              |35078 non-null:  float64| tax amount due for that property  |              ||              |
+|   fips                   |35078 non-null:  float64| unique ID for location of county   |              |
+|   propertylandusetypeid  |35078 non-null:  float64| unique ID use to identify property type (i.e. 261 - Single Family Residential)|              |
+|   propertylandusedesc    |35078 non-null:  object | Description of property type (i.e. Condominium, Townhome, Single Family Residential)|              |
+|   tax_rate               |35078 non-null:  float64| Tax rate for that property|              |
+|  county                 |35078 non-null:  object | County the property is located in|              |
+|  state                  |35078 non-null:  object | State the property is located in|              |
+
+
+
+
+DATA SCIENCE PIPELINE
+----------------------
+
+PLAN:
+-----
+
+PLAN -> Acquire -> Prepare -> Explore -> Model & Evaluate -> Deliver
+
+See my Trello board here --> https://trello.com/b/XcUxYoW0/zillow-clustering-project
+
+Working through the data science pipeline, I will acquire data using an acquire.py file which pulls data from the Zillow database using SQL and joins 3 tables. I will prepare the data using the wrangle.py file which will get rid of unneeded columns, rename columns and create columns for county, state and tax rates.
+Then I will explore the data by looking for possible relationships between features and look at how they are distribute by creating plots and looking at the data.
+Next I will create hypothesis and models. I will then compare the models that I ran on training data to validate data before running our model on the test data.   I will then present the findings in a verbal presentation using slides.  
+
+
+
+ACQUIRE:
+--------
+Call the acquire.py file within the wrangle.py to run the functions to obtain Zillow data using a SQL query from the Codeup Data Science Database: zillow
+It returns a pandas dataframe.  The SQL query joins two tables and filters on single family properties that I listed between May 1, 2017 to August 31, 2017.
+The data is filtering to return single unit properties defined within the propertylandusetypeid of codes: 261- Single Family Residential, 262 - Rural Residence, 
+263 - Mobile Home, 264 - Townhouse, 265 - Cluster Home, 266 - Condominium, and 279 - Inferred Single Family Residential.  
+
+
+    The SQL query that is run is shown below:
+                '''
+                 SELECT bedroomcnt, bathroomcnt, calculatedfinishedsquarefeet, taxvaluedollarcnt, yearbuilt, taxamount, fips, propertylandusetypeid, propertylandusedesc
+                FROM properties_2017
+                LEFT JOIN predictions_2017 USING (parcelid)
+                LEFT JOIN propertylandusetype USING (propertylandusetypeid)
+                WHERE propertylandusetypeid IN ('261','262','263','264','265','266','279') 
+                AND transactiondate BE IEN '2017-05-01' AND '2017-08-31';  
+                '''
+
+
+PREPARE:
+--------
+Prepped and cleaned the acquired data in the wrangle.py file.  Used functions within the wrangle.py to accomplish the following:
+
+- Nulls & duplicates were removed
+- Columns were renamed for easier use & readability
+- Outliers were removed per function using Interquartile Range (IQR) to find outliers for
+- A column named tax_rate was created by dividing taxamount and taxvaluedollarcnt
+- A column named county was created to name the county where the property is located (derived from fips code)
+- A column named state was created to name the state where the property is located (derived from fips code)
+- A prepped dataframe containing 35,078 entries and 12 columns
+- Split data into train, validate and test datasets.
+
+EXPLORE:
+--------
+Visualize combinations of variables to compare relationships between variables.
+    - Price seems to have a correlation to square footage, number of bedrooms, number of bathrooms, fips, and year built.
+
+Hypothesis 1:
+
+Null Hypothesis: Mean price for single family residential <= Mean of all single unit properties
+Alternative Hypothesis: Mean price for single family residential > Mean of monthly charges of all customers
+
+Finding: 
+
+    - We reject the null hypothesis that the mean price for single family residential <= Mean of all single unit properties
+
+Hypothesis 2:
+
+Null Hypothesis: There is no correlation between the square footage and price.
+Alternative Hypothesis: Square footage and price are correlated
+
+Finding: 
+    - We reject the null hypothesis that there is no correlation between the square footage and price.
+
+Hypothesis 3:
+
+Null Hypothesis: There is no correlation between number of bedrooms and location (fips).
+Alternative Hypothesis: Number of bedrooms and location (fips) are correlated.
+
+Finding: 
+    - We reject the null hypothesis that there is no correlation between number of bedrooms and location (fips).
+
+Hypothesis 4:
+
+Null Hypothesis: There is no correlation between number of bathrooms and price .
+Alternative Hypothesis: Number of bedrooms and price.
+
+Finding: 
+    - We reject the null hypothesis that there is no correlation between number of bathrooms and price .
+
+MODEL:
+------
+
+The goal is to develop a regression model that performs better than the baseline.
+
+Predicted all prices to be $418,560.92, which is equal to the mean of tax_value for the training sample.
+Predicted all final prices to be $359,073.00, which is equal to the median of tax_value for the training sample. 
+Computed the RMSE comparing actual final price (tax_value) to price_pred_mean.
+Computed the RMSE comparing actual final grade (tax_value) to price_pred_median
+
+
+Polynomial Regression Model performed better than the baseline by having a l Ir RMSE than the baseline
+The model explained 35.6% of the variance on out of sample data very similar to our validate dataset.
+
+DELIVER:
+-------
+
+- Present a report in the form of verbally supported slides.
+
+    The report/presentation slides should summarize findings about the drivers of the single unit property values. 
+
+
+- A github repository containing your work with any .py files required to acquire and prepare the data and a clearly labeled final Jupyter Notebook that walks through the pipeline.
+
+
+HOW TO RECREATE THIS PROJECT:
+----------------------------
+
+To recreate this project you will need the following files from this repository:
+- README.md
+- acquire.py
+- prepare.py
+- explore.py
+- final_zillow_regression_project.ipynb
+
+You will also need your own env file with your data base credentials to connect to the SQL database containing Zillow data 
+
+Instructions:
+- Read the README.md
+- Download the aquire.py, prepare.py, explore.py and final_zillow_regression_project.ipynb files into your working directory, or clone this repository
+- Add your own env file to your directory (user, password, host)  
+- Run the final_zillow_regression_project.ipynb 
+
+
+
+
+
+
---
+++ wrangle_excs.py
@@ -0,0 +1,276 @@
+#Zillow Clustering Project
+
+# 1. Acquire data from mySQL using the python module to connect and query. You will want to end with a single dataframe. Make sure to include: the logerror, all fields related to the properties that are available. You will end up using all the tables in the database.
+# Be sure to do the correct join (inner, outer, etc.). We do not want to eliminate properties purely because they may have a null value for airconditioningtypeid.
+# Only include properties with a transaction in 2017, and include only the last transaction for each property (so no duplicate property ID's), along with zestimate error and date of transaction.
+# Only include properties that include a latitude and longitude value.
+
+# Functions to obtain Zillow data from the Codeup Data Science Database: zillow
+#It returns a pandas dataframe.
+#--------------------------------
+
+# regular imports
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import os
+import env
+
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+
+
+# SQL Query to pull data from Zillow Database that joins all tables and returns all features for properties sold during 2017 with only latest transaction date
+# and excluding data that has null latitude and longitude
+sql = """
+SELECT prop.*,
+       pred.logerror,
+       pred.transactiondate,
+       air.airconditioningdesc,
+       arch.architecturalstyledesc,
+       build.buildingclassdesc,
+       heat.heatingorsystemdesc,
+       landuse.propertylandusedesc,
+       story.storydesc,
+       construct.typeconstructiondesc,
+       prop.regionidcity,
+       prop.regionidcounty,
+       prop.regionidneighborhood,
+       prop.regionidzip
+
+
+
+FROM   properties_2017 prop
+       INNER JOIN (SELECT parcelid,
+                   Max(transactiondate) transactiondate
+                   FROM   predictions_2017
+
+                   GROUP  BY parcelid) pred
+               USING (parcelid)
+               			JOIN predictions_2017 as pred USING (parcelid, transactiondate)
+       LEFT JOIN airconditioningtype air USING (airconditioningtypeid)
+       LEFT JOIN architecturalstyletype arch USING (architecturalstyletypeid)
+       LEFT JOIN buildingclasstype build USING (buildingclasstypeid)
+       LEFT JOIN heatingorsystemtype heat USING (heatingorsystemtypeid)
+       LEFT JOIN propertylandusetype landuse USING (propertylandusetypeid)
+       LEFT JOIN storytype story USING (storytypeid)
+       LEFT JOIN typeconstructiontype construct USING (typeconstructiontypeid)
+WHERE  prop.latitude IS NOT NULL
+       AND prop.longitude IS NOT NULL
+"""
+
+def get_db_url(database):
+    from env import host, user, password
+    url = f'mysql+pymysql://{user}:{password}@{host}/{database}'
+    return url
+
+
+# acquire zillow data using the query
+def get_zillow(sql):
+    url = get_db_url('zillow')
+    zillow_df = pd.read_sql(sql, url, index_col='id')
+    return zillow_df
+
+
+def handle_missing_values(df, prop_required_column = .5, prop_required_row = .70):
+	#function that will drop rows or columns based on the percent of values that are missing:\
+	#handle_missing_values(df, prop_required_column, prop_required_row
+    threshold = int(round(prop_required_column*len(df.index),0))
+    df = df.dropna(axis=1, thresh=threshold)
+    threshold = int(round(prop_required_row*len(df.columns),0))
+    df.dropna(axis=0, thresh=threshold, inplace=True)
+    return df
+
+
+def remove_columns(df, cols_to_remove):
+	#remove columns not needed
+    df = df.drop(columns=cols_to_remove)
+    return df
+
+def wrangle_zillow():
+    if os.path.isfile('zillow_cached.csv') == False:
+        df = get_zillow(sql)
+        df.to_csv('zillow_cached.csv',index = False)
+    else:
+        df = pd.read_csv('zillow_cached.csv')
+
+    # Restrict df to only properties that meet single use criteria
+    single_use = [261, 262, 263, 264, 265, 266, 279]
+    df = df[df.propertylandusetypeid.isin(single_use)]
+
+    # Restrict df to only those properties with at least 1 bath & bed and 350 sqft area
+    df = df[(df.bedroomcnt > 0) & (df.bathroomcnt > 0) & ((df.unitcnt<=1)|df.unitcnt.isnull())\
+            & (df.calculatedfinishedsquarefeet>350)]
+
+    # Handle missing values i.e. drop columns and rows based on a threshold
+    df = handle_missing_values(df)
+
+    # Add column for counties
+    df['county'] = df['fips'].apply(
+        lambda x: 'Los Angeles' if x == 6037\
+        else 'Orange' if x == 6059\
+        else 'Ventura')
+
+    # drop unnecessary columns
+    dropcols = ['parcelid',
+         'calculatedbathnbr',
+         'finishedsquarefeet12',
+         'fullbathcnt',
+         'heatingorsystemtypeid',
+         'propertycountylandusecode',
+         'propertylandusetypeid',
+         'propertyzoningdesc',
+         'censustractandblock'
+         ]
+
+    df = remove_columns(df, dropcols)
+
+    # replace nulls in unitcnt with 1
+    df.unitcnt.fillna(1, inplace = True)
+
+    # assume that since this is Southern CA, null means 'None' for heating system
+    df.heatingorsystemdesc.fillna('None', inplace = True)
+
+    # replace nulls with median values for select columns
+    df.lotsizesquarefeet.fillna(7313, inplace = True)
+    df.buildingqualitytypeid.fillna(6.0, inplace = True)
+
+    # Columns to look for outliers
+    #df = df[df.taxvaluedollarcnt < 5_000_000]
+    #df = df[df.calculatedfinishedsquarefeet < 8000]
+
+    #Remove outliers
+    df = remove_outliers(df, 2.99, ['calculatedfinishedsquarefeet',
+       'taxvaluedollarcnt','bedroomcnt', 'bathroomcnt'] )
+
+    # Drop any Nulls left
+    df = df.dropna()
+
+    # Rename
+    df = df.rename(columns = { 'bedroomcnt':'bedrooms', 
+                              'bathroomcnt':'bathrooms', 
+                              'calculatedfinishedsquarefeet':'sq_footage',
+                              'taxvaluedollarcnt':'tax_value', 
+                              'yearbuilt':'yr_built'
+        })  
+
+    return df
+
+def min_max_scaler(train, valid, test):
+    '''
+    Uses the train & test datasets created by the split_my_data function
+    Returns 3 items: mm_scaler, train_scaled_mm, test_scaled_mm
+    This is a linear transformation. Values will lie between 0 and 1
+    '''
+    num_vars = list(train.select_dtypes('number').columns)
+    scaler = MinMaxScaler()
+    train[num_vars] = scaler.fit_transform(train[num_vars])
+    valid[num_vars] = scaler.transform(valid[num_vars])
+    test[num_vars] = scaler.transform(test[num_vars])
+    return scaler, train, valid, test
+
+def outlier_function(df, cols, k):
+	#function to detect and handle oulier using IQR rule
+    for col in df[cols]:
+        q1 = df.annual_income.quantile(0.25)
+        q3 = df.annual_income.quantile(0.75)
+        iqr = q3 - q1
+        upper_bound =  q3 + k * iqr
+        lower_bound =  q1 - k * iqr
+        df = df[(df[col] < upper_bound) & (df[col] > lower_bound)]
+    return df
+
+def train_validate_test_split(df):
+    train_and_validate, test = train_test_split(df, train_size=0.8, random_state=123)
+    train, validate = train_test_split(train_and_validate, train_size=0.75, random_state=123)
+    return train, validate, test
+
+
+def nulls_by_col(df):
+    num_missing = df.isnull().sum()
+    rows = df.shape[0]
+    prcnt_miss = num_missing / rows * 100
+    cols_missing = pd.DataFrame({'num_rows_missing': num_missing, 'percent_rows_missing': prcnt_miss})
+    return cols_missing
+
+def nulls_by_row(df):
+    num_missing = df.isnull().sum(axis=1)
+    prcnt_miss = num_missing / df.shape[1] * 100
+    rows_missing = pd.DataFrame({'num_cols_missing': num_missing, 'percent_cols_missing': prcnt_miss})\
+    .reset_index()\
+    .groupby(['num_cols_missing', 'percent_cols_missing']).count()\
+    .rename(index=str, columns={'customer_id': 'num_rows'}).reset_index()
+    return rows_missing
+
+def summarize(df):
+    '''
+    summarize will take in a single argument (a pandas dataframe)
+    and output to console various statistics on said dataframe, including:
+    # .head()
+    # .info()
+    # .describe()
+    # value_counts()
+    # observation of nulls in the dataframe
+    '''
+    print('=====================================================\n\n')
+    print('Dataframe head: ')
+    print(df.head(3).to_markdown())
+    print('=====================================================\n\n')
+    print('Dataframe info: ')
+    print(df.info())
+    print('=====================================================\n\n')
+    print('Dataframe Description: ')
+    print(df.describe().to_markdown())
+    num_cols = [col for col in df.columns if df[col].dtype != 'O']
+    cat_cols = [col for col in df.columns if col not in num_cols]
+    print('=====================================================')
+    print('DataFrame value counts: ')
+    for col in df.columns:
+        if col in cat_cols:
+            print(df[col].value_counts())
+        else:
+            print(df[col].value_counts(bins=10, sort=False))
+    print('=====================================================')
+    print('nulls in dataframe by column: ')
+    print(nulls_by_col(df))
+    print('=====================================================')
+    print('nulls in dataframe by row: ')
+    print(nulls_by_row(df))
+    print('============================================')
+
+# FUNCTION to remove outliers per John Salas
+# ------------------------------------------
+#   The value for k is a constant that sets the threshold. 
+#   Usually, you’ll see k start at 1.5, or 3 or less, depending on how many outliers you want to keep. 
+#   The higher the k, the more outliers you keep. Recommend not going beneath 1.5, but this is worth using, 
+#   especially with data w/ extreme high/low values
+
+def remove_outliers(df, k, col_list):
+    ''' remove outliers from a list of columns in a dataframe 
+        and return that dataframe
+    '''
+    
+    for col in col_list:
+
+        q1, q3 = df[col].quantile([.25, .75])  # get quartiles
+        
+        iqr = q3 - q1   # calculate interquartile range
+        
+        upper_bound = q3 + k * iqr   # get upper bound
+        lower_bound = q1 - k * iqr   # get lower bound
+
+        # return dataframe without outliers
+        
+        df = df[(df[col] > lower_bound) & (df[col] < upper_bound)]
+        
+    return df
+
+
+#FUNCTION to get min/max from columns in a dataframe
+# --------------------------------------------------
+# To call this function use:   df.apply(minMax)  (Outside will have to call as:  df.apply(wrangle.minMax))
+# x is a list dataframe or 
+def minMax(x):
+    return pd.Series(index=['min','max'],data=[x.min(),x.max()])
